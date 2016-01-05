edbddf9aff45664f7e128e75d6ca603cb3dbc447
wertrain <wertrain@gmail.com>
2016-01-05 12:14:24 +0900

    add files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default', ['webserver']);
