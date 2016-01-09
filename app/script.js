7cb3665cdc9b850a7bf9beb3a9b0065b956ed47e
wertrain <wertrain@gmail.com>
2016-01-09 23:03:59 +0900

    WIP

--- app/script.js
+++ app/script.js
@@ -25,6 +25,14 @@ var csv2json = function(csvText) {
     var LABEL_ADJUSTMENT = labelIndex++;
     var LABEL_BEFORE_CONVERSION = labelIndex++;
     var LABEL_AGGREGATE = labelIndex++;
+    
+    var amountCategories = [];
+    for (i = 0; i < data.length; ++i) {
+        if (typeof amountCategories[data[i][LABEL_CATEGORY]] === 'undefined') {
+            amountCategories[data[i][LABEL_CATEGORY]] = parseInt(data[i][LABEL_SPENDING], 10);
+        }
+        console.log();
+    }
 };
 
 var init = function() {
@@ -72,49 +80,55 @@ var init = function() {
     });
     
     $('#highcharts-container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
         title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
+            text: 'ジャンルごとの総計'
         },
         tooltip: {
-            valueSuffix: '°C'
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
         },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
         },
         series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Microsoft Internet Explorer',
+                y: 56.33
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Firefox',
+                y: 10.38
+            }, {
+                name: 'Safari',
+                y: 4.77
+            }, {
+                name: 'Opera',
+                y: 0.91
+            }, {
+                name: 'Proprietary or Undetectable',
+                y: 0.2
+            }]
         }]
     });
 }();
